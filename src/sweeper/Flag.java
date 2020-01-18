1112d4f6fb7ade1abea95121a9803f441736a4e3
Worst_Developer_Ever <xsonx@yandex.ru>
2020-01-18 21:29:02 +0300

    first commit

---
+++ src/sweeper/Flag.java
@@ -0,0 +1,73 @@
+package sweeper;
+
+class Flag
+{
+    private Matrix flagMap;
+    private int countOfClosedBoxes;
+
+    void start()
+    {
+        flagMap = new Matrix(Box.CLOSED);
+        countOfClosedBoxes = Ranges.getSize().x * Ranges.getSize().y;
+    }
+
+    Box get(Coord coord)
+    {
+        return flagMap.get(coord);
+    }
+
+    void setOpenedToBox(Coord coord)
+    {
+        flagMap.set(coord, Box.OPENED);
+        countOfClosedBoxes --;
+    }
+
+    void toggleFlagetToBox(Coord coord)
+    {
+        switch (flagMap.get(coord))
+        {
+            case FLAGED: setClosedToBox (coord); break;
+            case CLOSED: setFlagetToBox(coord); break;
+        }
+    }
+
+    private void setClosedToBox(Coord coord)
+    {
+        flagMap.set(coord, Box.CLOSED);
+    }
+
+    private void setFlagetToBox(Coord coord)
+    {
+        flagMap.set(coord, Box.FLAGED);
+    }
+
+    int getCountOfClosedBoxes()
+    {
+        return countOfClosedBoxes;
+    }
+
+    void setBombedToBox(Coord coord)
+    {
+        flagMap.set(coord, Box.BOMBED);
+    }
+
+    void setOpenedToCloseBombBox(Coord coord)
+    {
+        if (flagMap.get(coord) == Box.CLOSED)
+            flagMap.set(coord, Box.OPENED);
+    }
+
+    void setNoBombToFlagedSafeBox(Coord coord)
+    {
+        if (flagMap.get(coord) == Box.FLAGED)
+            flagMap.set(coord, Box.NOBOMB);
+    }
+
+   int getCountOfFlagedBoxesAround(Coord coord) {
+        int count = 0;
+        for (Coord around: Ranges.getCoordsAround(coord))
+            if (flagMap.get(around) == Box.FLAGED)
+                count++;
+            return count;
+    }
+}
